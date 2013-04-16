41286e1748f1397b959fca36433686810281c808
florapdx <floraworleypdx@gmail.com>
2013-04-16 02:52:56 -0700

    added functions to handle new pages, and stubbed functions to handle errors

--- sitebuilder.py
+++ sitebuilder.py
@@ -1,9 +1,13 @@
 import sys, shutil
 
 from flask import Flask, render_template, url_for, redirect, abort
-from flask_flatpages import FlatPages
+from flask_flatpages import FlatPages, pygments_style_defs
 from flask_frozen import Freezer
 from flask.ext.assets import Environment
+#from pygments import highlight
+#from pygments.formatters import HtmlFormatter
+from datetime import date, datetime
+
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -27,12 +31,46 @@ def index():
 	posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
 	return render_template('index.html', posts=posts)
 
+@app.route('/tutorials/')
+def tutorials():
+	""" Show a list of most recent posts on index page """
+	posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+	return render_template('tutorials.html', posts=posts)
+
+@app.route('/community/')
+def community():
+	""" Show a list of most recent posts on index page """
+	posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+	return render_template('community.html', posts=posts)
+
+@app.route('/events/')
+def events():
+	""" Show a list of most recent posts on index page """
+	posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+	return render_template('events.html', posts=posts)
+
+@app.route('/resources/')
+def resources():
+	""" Show a list of most recent posts on index page """
+	return render_template('resources.html')
+
+@app.route('/about/')
+def about():
+	""" Show a list of most recent posts on index page """
+	return render_template('about.html')
+
+@app.route('/portfolio/')
+def portfolio():
+	""" Show a list of most recent posts on index page """
+	return render_template('portfolio.html')
+
+
 @app.route('/pages/<name>')
 def page(name):
 	""" render static pages """
 	path = '{}/{}'.format(PAGE_DIR, name)
 	page = flatpages.get_or_404(path)
-	return render_template('page.html', page=page)
+	return render_template('page.html', page=page, pages=pages)
 
 @app.route('/posts/<name>')
 def post(name):
@@ -46,11 +84,24 @@ def tag(tag):
     tagged = [p for p in pages if tag in p.meta.get('tags', [])]
     return render_template('tag.html', pages=tagged, tag=tag)
 
-"""
+@app.route('/events/<name>')
+def event(name):
+	""" render events """
+	path = '{}/{}'.format(EVENT_DIR, name)
+	event = flatpages.get_or_404(path)
+	return render_template('event.html', event=event)
+
 @app.route('/pygments.css')
 def pygments_css():
-	return pygments_style_defs('manni'), 200, {'Content-Type': 'text/css'}
-"""
+    return pygments_style_defs('manni'), 200, {'Content-Type': 'text/css'}
+
+@app.route('/404.html')
+def error404():
+	return render_template('404.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template('404.html'), 404
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "build":
